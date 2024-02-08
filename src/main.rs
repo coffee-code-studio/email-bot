4a62e3e303b192728731f389e7b35e4b6a5ec0a7
Tim Reed <reed11tim@gmail.com>
2024-02-08 03:34:58 -0500

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::fs::File;
 use std::io::Write;
 use std::io;
+use std::collections::HashSet;
 use reqwest;
+use regex::Regex;
 use scraper::{Html, Selector};
 use serde::{Serialize, Deserialize};
 use serde_json;
-use lettre::{Message, SmtpTransport, Transport};
+use lettre::{Message, SmtpTransport, Transport, message::header::ContentType};
 use lettre::transport::smtp::authentication::Credentials;
 use askama::Template;
 use redis::Commands;
@@ -54,10 +56,6 @@ struct Business {
 #[template(path = "email_template.html")]
 struct EmailTemplate {
     subject: String,
-    name: String,
-    position: String,
-    contact_information: String,
-    website_url: String,
 }
 
 fn current_day() -> String {
@@ -71,9 +69,8 @@ fn check_update_email_count(con: &mut redis::Connection, max_emails_per_day: usi
     let max_retries = 5;
 
     loop {
-        match con.get(&key) {
-            Ok(count) => {
-                let count: isize = count;
+        match con.get::<_, Option<isize>>(&key) {
+            Ok(Some(count)) => {
                 if count < max_emails_per_day as isize {
                     let _: () = con.incr(&key, 1).map_err(BotError::RedisError)?;
                     if count == 0 {
@@ -84,13 +81,19 @@ fn check_update_email_count(con: &mut redis::Connection, max_emails_per_day: usi
                     return Ok(false);
                 }
             },
+            Ok(None) => {
+                // Key does not exist, so we treat it as 0 and attempt to set it to 1 and add an expiration
+                let _: () = con.set(&key, 1).map_err(BotError::RedisError)?;
+                let _: () = con.expire(&key, 86400).map_err(BotError::RedisError)?;
+                return Ok(true);
+            },
             Err(err) => {
                 if retry_count >= max_retries {
                     return Err(BotError::RedisError(err));
                 } else {
                     eprintln!("Redis failed, retrying... (Attempt: {})", retry_count + 1);
                     retry_count += 1;
-                    thread::sleep(Duration::from_millis(500)); // Wait for 500 ms
+                    thread::sleep(Duration::from_millis(500)); // Wait for 500 ms before retrying
                 }
             }
         }
@@ -100,6 +103,7 @@ fn check_update_email_count(con: &mut redis::Connection, max_emails_per_day: usi
 #[tokio::main]
 async fn main() -> Result<(), BotError> {
     let client = reqwest::Client::new();
+    let mut processed_emails: HashSet<String> = HashSet::new();
     let mut page_number = 1;
     let mut has_more_pages = true;
     let mut businesses: Vec<Business> = Vec::new();
@@ -112,7 +116,7 @@ async fn main() -> Result<(), BotError> {
 
     while has_more_pages {
         let list_page_url = format!(
-            "https://www.yellowpages.com/search?search_terms=event+planning&geo_location_terms=Columbus%2C+OH&page={}",
+            "https://www.yellowpages.com/search?search_terms=Landscaping+%26+Lawn+Services&geo_location_terms=Columbus%2C+OH&page={}",
             page_number
         );
 
@@ -129,14 +133,7 @@ async fn main() -> Result<(), BotError> {
         let business_links: Vec<_> = list_page_document.select(&business_link_selector).collect();
 
         if business_links.is_empty() {
-            // has_more_pages = false;
             break;
-        }
-
-        has_more_pages = !business_links.is_empty();
-
-        if has_more_pages {
-            page_number += 1;
         }
 
         for link_element in business_links {
@@ -152,18 +149,38 @@ async fn main() -> Result<(), BotError> {
                 
                 let detail_page_document = Html::parse_document(&detail_page_response);
                 let email_selector = Selector::parse("a.email-business").unwrap();
-
+        
                 if let Some(email_element) = detail_page_document.select(&email_selector).next() {
-                    let email = email_element.inner_html();
-                    println!("Business URL: {}", detail_url);
-                    println!("Business Email: {}", email);
-
-                    businesses.push(Business {
-                        url: detail_url,
-                        email: email,
-                    });
+                    let email = if let Some(email_href) = email_element.value().attr("href") {
+                        if email_href.starts_with("mailto:") {
+                            let re = Regex::new(r"mailto:([^?]+)").unwrap();
+                            if let Some(caps) = re.captures(email_href) {
+                                caps.get(1).map_or("", |m| m.as_str()).to_string()
+                            } else {
+                                "".to_string()
+                            }
+                        } else {
+                            email_href.to_string()
+                        }
+                    } else {
+                        email_element.inner_html()
+                    };
+                
+                    if !processed_emails.contains(&email) {
+                        processed_emails.insert(email.clone());
+        
+                        println!("Business URL: {}", detail_url);
+                        println!("Business Email: {}", email);
+        
+                        businesses.push(Business {
+                            url: detail_url,
+                            email: email.clone(),
+                        });
+                    } else {
+                        println!("Duplicate email found, skipping: {}", email);
+                    }
                 }
-
+        
                 tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
             }
         }
@@ -176,24 +193,29 @@ async fn main() -> Result<(), BotError> {
     file.write_all(json_data.as_bytes()).map_err(BotError::IOError)?;
 
     let email_sender = "coffeecodestudio.dev@gmail.com";
-    let creds = Credentials::new(email_sender.to_string(), "<add password>".to_string());
+    let creds = Credentials::new(email_sender.to_string(), "mquh ggvs flcn bilf".to_string());
     let mailer = SmtpTransport::relay("smtp.gmail.com")?.credentials(creds).build();
     
     let subject = "Grow Your Business with Coffee Code Studio - Special Offer Inside!".to_string();
-    let name = "Tim Reed".to_string();
-    let position = "Lead Developer".to_string();
-    let contact_information = "coffeecodestudio.dev@gmail.com".to_string();
-    let website_url = "https://coffeecodestudio.com/".to_string();
 
     let email_template = EmailTemplate {
         subject: subject.clone(),
-        name,
-        position,
-        contact_information,
-        website_url,
     };
 
     let email_content = email_template.render().map_err(BotError::TemplateError)?;
+
+    println!("Email content preview:");
+    println!("Subject: {}", subject);
+    println!("Content: {}", email_content);
+    println!("-------------------------");
+
+    println!("Do you want to proceed with sending emails? (yes/no):");
+    let mut confirmation = String::new();
+    std::io::stdin().read_line(&mut confirmation).map_err(BotError::IOError)?;
+    if confirmation.trim().to_lowercase() != "yes" {
+        println!("Aborted by user.");
+        return Ok(());
+    }
 
     for business in &businesses {
         if !business.email.contains('@') {
@@ -206,8 +228,10 @@ async fn main() -> Result<(), BotError> {
                 .from(email_sender.parse().unwrap())
                 .to(business.email.parse().unwrap())
                 .subject(&subject)
+                .header(ContentType::TEXT_HTML) 
                 .body(email_content.clone())
                 .map_err(BotError::EmailError)?;
+
 
             match mailer.send(&email) {
                 Ok(_) => println!("Email sent successfully to: {}", business.email),
